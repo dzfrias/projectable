38a225dd505d3422e391a29f8a3097abfcfededf
Diego <96022404+dzfrias@users.noreply.github.com>
2023-03-25 21:53:57 -0700

    chore(files): add poll interval for watcher

--- src/external_event/refresh.rs
+++ src/external_event/refresh.rs
@@ -1,18 +1,18 @@
-use crossbeam_channel::{unbounded, Sender};
-use std::{path::Path, thread};
-
 use super::ExternalEvent;
 use anyhow::Result;
-use notify::{recommended_watcher, EventKind, RecommendedWatcher, RecursiveMode, Watcher};
+use crossbeam_channel::{unbounded, Sender};
+use notify::{recommended_watcher, Config, EventKind, RecommendedWatcher, RecursiveMode, Watcher};
+use std::{path::Path, thread, time::Duration};
 
 /// Watch for changes to the filesystem at `path`, sending results to `event_sender`
 pub fn fs_watch(
     path: &Path,
     event_sender: Sender<ExternalEvent>,
-    _refresh_time: u64,
+    refresh_time: u64,
 ) -> Result<RecommendedWatcher> {
     let (tx, rx) = unbounded();
     let mut watcher = recommended_watcher(tx)?;
+    watcher.configure(Config::default().with_poll_interval(Duration::from_millis(refresh_time)))?;
     watcher.watch(path, RecursiveMode::Recursive)?;
     thread::spawn(move || {
         for res in rx {
